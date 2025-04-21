e08943568db8bcdf3c1bf95770dfde6a363d6f1b
Hurtadx <andreshm253@gmail.com>
2025-04-21 16:32:27 -0500

    fix: add error handling for profile picture loading in PostHeader component

--- PostFacebook/src/Components/Post/PostHeader.jsx
+++ PostFacebook/src/Components/Post/PostHeader.jsx
@@ -1,14 +1,26 @@
-function PostHeader({username, profilePic,timestamp}){
+function PostHeader({username, profilePic, timestamp}){
+   
+    const handleImageError = (e) => {
+        e.target.src = "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y";
+    };
+
     return(
         <div className="post-header">
-            <img src={profilePic} alt="Profile" className="profile-pic" />
+            <img 
+                src={profilePic} 
+                alt="Profile" 
+                className="profile-pic" 
+                onError={handleImageError}
+            />
             <div className="post-info">
-                <h3 className="userName">{username}</h3>
+                <h3 className="username">{username}</h3>
                 <span className="timestamp">{timestamp}</span>
             </div>
+            <button className="post-header-options">
+                <i className="fa-solid fa-ellipsis"></i>
+            </button>
         </div>
     );
-
 }
 
 export default PostHeader;
